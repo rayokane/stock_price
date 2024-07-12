06aa51c76c52a831e324ed6ef9b51b3008decf25
rayokane <34676117+rayokane@users.noreply.github.com>
2024-07-12 13:21:10 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -9,7 +9,7 @@ st.write("""
 
 # Simple Stock Price App
 
-Shown are the stock closing price and volume of Apple.
+Shown are the stock closing price and volume of Google.
 """)
 
 # https://towardsdatascience.comhow-to-get-stock-data-using-python-c0de1df17e75
